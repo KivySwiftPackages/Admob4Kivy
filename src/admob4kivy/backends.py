391db8f7af23c4e5cea4ea79633e07f6f9d253d1
PythonSwiftLink <107263226+PythonSwiftLink@users.noreply.github.com>
2025-08-06 12:42:13 +0200

    case fix

--- src/admob4kivy/backends.py
+++ src/admob4kivy/backends.py
@@ -5,7 +5,7 @@ admob_backend = environ.get('A4K_BACKEND')
 match admob_backend:
     case "pyjnius":
         from a4k_pyjnius import AdmobBackend
-    case "pyswift"
+    case "pyswift":
         from a4k_pyswift import AdmobBackend # type: ignore
     case _:
         raise ValueError(f"Unsupported backend: {admob_backend}. Supported backends: 'android'.")
